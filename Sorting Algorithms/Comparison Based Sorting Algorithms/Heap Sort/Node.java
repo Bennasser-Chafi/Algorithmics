8049a0750a941e3a003104e50c3153dfd3c11fc3
BENNASSAR <chafi.bennassar@capgemini.com>
2018-07-16 14:06:57 +0100

    commit

---
+++ Sorting Algorithms/Comparison Based Sorting Algorithms/Heap Sort/Node.java
@@ -0,0 +1,20 @@
+/**
+ * This class represents a single node in the heap data structure
+ */
+
+public class Node {
+    
+    private int key;
+
+    public Node(int key) {
+        this.key = key;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+}
